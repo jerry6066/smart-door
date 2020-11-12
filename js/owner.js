9412e57b0924fc7abaddf6ea40c00dcfe42ad3ef
Libo Jian <1220266538@qq.com>
2020-11-12 00:39:23 -0500

    modify

--- js/owner.js
+++ js/owner.js
@@ -1,42 +1,58 @@
 const url = "https://64hhto6qci.execute-api.us-east-1.amazonaws.com/testStage/add-visitor";
 
-/*
-$('.visitor-img').on('error',function() {
+/*$('.visitor-img').on('error', function() {
+  console.log("error");
   $('.visitor-img').attr('src', 'https://visitor-image.s3.amazonaws.com/noPeople.jpg');
   $('input').prop('disabled', true);
   $('button').prop('disabled', true);
-  console.log("error");
 });*/
 
-$(document).ready(function(){
- $("img").bind("error",function(){
-  // Replacing image source
-  $(this).attr("src",'https://visitor-image.s3.amazonaws.com/noPeople.jpg');
-  $('input').prop('disabled', true);
-  $('button').prop('disabled', true);
-  console.log("error");
- });
-});
-
-$('#submitButton').click(function() {
-  var data = {
-    "permission": true,
-    "name": $('#nameInput').val(),
-    "phone_number": $('#phoneInput').val()
-  };
+$(document).ready(function() {
   $.ajax({
-    url: url,
+    url: "https://64hhto6qci.execute-api.us-east-1.amazonaws.com/testStage/checkfile",
     crossDomain: true,
-    type: "POST",
+    type: "GET",
     headers: {
       'Content-Type': 'application/json'
     },
-    data: JSON.stringify(data),
-    dataType: "json"
   }).done(function(response) {
     console.log(response);
-    window.location.href = "https://smart-door.onrender.com/allowed";
+    if (response.status === true) {
+      $('.visitor-img').attr('src', 'https://visitor-image.s3.amazonaws.com/new_visitor.jpeg');
+      $('input').prop('disabled', false);
+      $('button').prop('disabled', false);
+    } else {
+      $('.visitor-img').attr('src', 'https://visitor-image.s3.amazonaws.com/noPeople.jpg');
+      $('input').prop('disabled', true);
+      $('button').prop('disabled', true);
+    }
   });
+});
+
+$('#submitButton').click(function() {
+  if ($('#nameInput').val() === "" || $('#phoneInput').val() === "") {
+    alert("Please type in all the information.")
+  } else {
+    var data = {
+      "permission": true,
+      "name": $('#nameInput').val(),
+      "phone_number": $('#phoneInput').val()
+    };
+    $.ajax({
+      url: url,
+      crossDomain: true,
+      type: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: JSON.stringify(data),
+      dataType: "json"
+    }).done(function(response) {
+      console.log(response);
+      window.location.href = "https://smart-door.onrender.com/allowed";
+    });
+  }
+
 })
 
 $('#denyButton').click(function() {
